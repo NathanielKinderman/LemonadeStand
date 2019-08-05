17f2a58d90325c37c614a942153c16dcb7e83905
Nathaniel Kinderman <natekinderman16@gmail.com>
2019-08-05 11:40:11 -0500

    Created classes and layed out blueprint to work off of.

---
+++ LemonadeStand/LemonadeStand/Store.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Store
+    {
+        //member variable(has a)
+
+
+
+        //constructor
+
+
+
+        //member method(can do)
+    }
+}
